2d3e081971ceb10fa1485ecc0a23a2d0362707fd
Ilya111Kudryashov <44931669+Ilya111Kudryashov@users.noreply.github.com>
2024-10-07 15:16:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -49,19 +49,7 @@ const benefitSwiperConfig={
   },
 
 }
-// const swiper = new Swiper('#mainBenefitSlider', {
-  
 
-//   pagination: {
-//     el: '#mainBenefitpagination',
-//   },
-// 	effect: "fade",
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-// });
 
 let benefit_swiper;
 
@@ -82,7 +70,7 @@ breakpoint.addListener(breakpointChecker);
 breakpointChecker();
 
 
-
+//resize
 jQuery(($) => {
   if ($(window).width() > 1191) {
     $('.header__menu-item--hasSub').hover(
@@ -96,6 +84,80 @@ jQuery(($) => {
       $(this).toggleClass('menu__item-subMobile')
       $(this).removeClass('menu__item--active');
     })
+  }
+  if ($(window).width() < 481) {
+    $('.hwork__content').addClass('scrolling_wrapper')
+    $('.hwork__content-item').addClass('uc-leftscroll')
+
+
+      $('.hwork__content').mousewheel(function(e, delta) {
+          // multiplying by 40 is the sensitivity, 
+          // increase to scroll faster.
+          this.scrollLeft -= (delta * 40);
+          e.preventDefault();
+      });
+
+    // initMagicScroll();
+    // function initMagicScroll() {
+    //     const groups = [];
+    //     $('.uc-rightscroll, .uc-leftscroll').each(function() {
+    //         if ($(this).prev().hasClass('uc-rightscroll') && $(this).hasClass('uc-rightscroll')) {
+    //             groups[groups.length - 1].$els = groups.at(-1).$els.add($(this));
+    //             return;
+    //         }
+
+    //          if ($(this).prev().hasClass('uc-leftscroll') && $(this).hasClass('uc-leftscroll')) {
+    //             groups[groups.length - 1].$els = groups.at(-1).$els.add($(this));
+    //             return;
+    //         }
+    
+    //         groups.push({
+    //             $els: $(this),
+    //             options: {
+    //                 dir: $(this).hasClass('uc-leftscroll') ? 'left' : 'right'
+    //             }
+    //         });
+    //     });
+    
+    //     const map = {};
+    //     for (let group of groups) {
+    //         const id = 'uc' + Date.now();
+    //         map[id] = group;
+    //         const extraStyle = group.options.dir === 'right' ? `left: -${group.$els.length - 1}00px; flex-direction: row-reverse;` : '';
+    //         group
+    //             .$els
+    //             .css({
+    //                 flex: '0 0 238px',
+    //                 marginRight:"61px",
+    //             })
+    //             .wrapAll(`<div id="pin-${id}" style="overflow: hidden; width: 100vw; height: 45vh;"><div id="${id}" style="display: flex; width: ${group.$els.length}00px; height: 100%; position: relative; ${extraStyle}"></div></div>`);
+    //     }
+    
+    //     const controller = new ScrollMagic.Controller();
+    
+    //     for (let id in map) {
+    //         const delta = 100 - 100 / map[id].$els.length;
+    //         const sign = (map[id].options.dir === 'left') ? '-' : '';
+    //         const animation = new TimelineMax().to(`#${id}`, 1, {x: `${sign}${delta}%`, ease: Linear.easeNone});//260
+
+    //         new ScrollMagic.Scene({
+    //             triggerElement: `#pin-${id}`,
+    //             triggerHook: "onLeave",
+    //             duration: `${map[id].$els.length}00%`
+    //         })
+    //         .setPin(`#pin-${id}`)
+    //         .setTween(animation)
+    //         .addTo(controller);    
+    //     }    
+    // }
+
+
+
+
+
+  }else{
+    $('.hwork__content').removeClass('scrolling_wrapper')
+    $('.hwork__content-item').removeClass('uc-leftscroll')
   }
 });
 
@@ -149,140 +211,59 @@ burgerBtn.addEventListener('click',function(){
 
 
 
-// let sliderTabs = document.querySelector('.scrolling_wrapper');
-
-// sliderTabs.addEventListener('wheel', function(event) {
-
-//     event.preventDefault();
-
-//     // 120 длина вкладки, выходящей за пределы экрана.
-
-//     if(sliderTabs.scrollLeft === 0) sliderTabs.scrollLeft += 120;   
-//     else sliderTabs.scrollLeft -= 120;
-// });
-
-// // Автоскрол при нажатии на вкладки
-
-// let tabs = document.querySelectorAll('.scrolling_wrapper');
-
-// tabs.forEach((item) => {
-//     item.onclick = function() {
-//         if(item.textContent == 'Вкладка за пределами экрана') {
-//             sliderTabs.scrollLeft += 120
-//         }
-//         if(item.textContent == 'Первая вкладка') {
-//             sliderTabs.scrollLeft -= 120
-//         }
-//     };
-// });
-// const scrollContainer = document.querySelector(".scrolling_wrapper");
-
-// scrollContainer.addEventListener("wheel", (evt) => {
-//     evt.preventDefault();
-//     scrollContainer.scrollLeft += evt.deltaY;
-// });
 
 
-// /**
-// * By Alvaro Trigo 
-// * Follow me on Twitter: https://twitter.com/imac2
-// */
-// (function(){
-//   init();
 
-//   function init(){
-//       setStickyContainersSize();
-//       bindEvents();
-//   }
+ //try magic scroll
+    // initMagicScroll();
+    // function initMagicScroll() {
+    //     const groups = [];
+    //     $('.uc-rightscroll, .uc-leftscroll').each(function() {
+    //         if ($(this).prev().hasClass('uc-rightscroll') && $(this).hasClass('uc-rightscroll')) {
+    //             groups[groups.length - 1].$els = groups.at(-1).$els.add($(this));
+    //             return;
+    //         }
 
-//   function bindEvents(){
-//       window.addEventListener("wheel", wheelHandler);        
-//   }
-
-//   function setStickyContainersSize(){
-//       document.querySelectorAll('.scrolling_wrapper .card').forEach(function(container){
-//           const stikyContainerHeight = container.querySelector('.scrolling_wrapper').scrollWidth;
-//           container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
-//       });
-//   }
-
-//   function isElementInViewport (el) {
-//       const rect = el.getBoundingClientRect();
-//       return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
-//   }
-
-//   function wheelHandler(evt){
-      
-//       const containerInViewPort = Array.from(document.querySelectorAll('.scrolling_wrapper .card')).filter(function(container){
-//           return isElementInViewport(container);
-//       })[0];
-
-//       if(!containerInViewPort){
-//           return;
-//       }
-
-//       var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
-//       var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
-//       let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
-
-//       if(g_canScrollHorizontally){
-//           containerInViewPort.querySelector('.scrolling_wrapper').scrollLeft += evt.deltaY;
-//       }
-//   }
-// })();
-
-
-{/* <script> */}
-    initMagicScroll();
-    function initMagicScroll() {
-        const groups = [];
-        $('.uc-rightscroll, .uc-leftscroll').each(function() {
-            if ($(this).prev().hasClass('uc-rightscroll') && $(this).hasClass('uc-rightscroll')) {
-                groups[groups.length - 1].$els = groups.at(-1).$els.add($(this));
-                return;
-            }
-
-             if ($(this).prev().hasClass('uc-leftscroll') && $(this).hasClass('uc-leftscroll')) {
-                groups[groups.length - 1].$els = groups.at(-1).$els.add($(this));
-                return;
-            }
+    //          if ($(this).prev().hasClass('uc-leftscroll') && $(this).hasClass('uc-leftscroll')) {
+    //             groups[groups.length - 1].$els = groups.at(-1).$els.add($(this));
+    //             return;
+    //         }
     
-            groups.push({
-                $els: $(this),
-                options: {
-                    dir: $(this).hasClass('uc-leftscroll') ? 'left' : 'right'
-                }
-            });
-        });
+    //         groups.push({
+    //             $els: $(this),
+    //             options: {
+    //                 dir: $(this).hasClass('uc-leftscroll') ? 'left' : 'right'
+    //             }
+    //         });
+    //     });
     
-        const map = {};
-        for (let group of groups) {
-            const id = 'uc' + Date.now();
-            map[id] = group;
-            const extraStyle = group.options.dir === 'right' ? `left: -${group.$els.length - 1}00px; flex-direction: row-reverse;` : '';
-            group
-                .$els
-                .css({
-                    flex: '0 0',
-                })
-                .wrapAll(`<div id="pin-${id}" style="overflow: hidden; width: 100vw; height: 100vh;"><div id="${id}" style="display: flex; width: ${group.$els.length}00px; height: 100%; position: relative; ${extraStyle}"></div></div>`);
-        }
+    //     const map = {};
+    //     for (let group of groups) {
+    //         const id = 'uc' + Date.now();
+    //         map[id] = group;
+    //         const extraStyle = group.options.dir === 'right' ? `left: -${group.$els.length - 1}00px; flex-direction: row-reverse;` : '';
+    //         group
+    //             .$els
+    //             .css({
+    //                 flex: '0 0',
+    //             })
+    //             .wrapAll(`<div id="pin-${id}" style="overflow: hidden; width: 100vw; height: 100vh;"><div id="${id}" style="display: flex; width: ${group.$els.length}00px; height: 100%; position: relative; ${extraStyle}"></div></div>`);
+    //     }
     
-        const controller = new ScrollMagic.Controller();
+    //     const controller = new ScrollMagic.Controller();
     
-        for (let id in map) {
-            const delta = 100 - 100 / map[id].$els.length;
-            const sign = (map[id].options.dir === 'left') ? '-' : '';
-            const animation = new TimelineMax().to(`#${id}`, 1, {x: `${sign}${delta}%`, ease: Linear.easeNone});
+    //     for (let id in map) {
+    //         const delta = 100 - 100 / map[id].$els.length;
+    //         const sign = (map[id].options.dir === 'left') ? '-' : '';
+    //         const animation = new TimelineMax().to(`#${id}`, 1, {x: `${sign}${delta}%`, ease: Linear.easeNone});
 
-            new ScrollMagic.Scene({
-                triggerElement: `#pin-${id}`,
-                triggerHook: "onLeave",
-                duration: `${map[id].$els.length}00%`
-            })
-            .setPin(`#pin-${id}`)
-            .setTween(animation)
-            .addTo(controller);    
-        }    
-    }
-// </script>
+    //         new ScrollMagic.Scene({
+    //             triggerElement: `#pin-${id}`,
+    //             triggerHook: "onLeave",
+    //             duration: `${map[id].$els.length}00%`
+    //         })
+    //         .setPin(`#pin-${id}`)
+    //         .setTween(animation)
+    //         .addTo(controller);    
+    //     }    
+    // }
